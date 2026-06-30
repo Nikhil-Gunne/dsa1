beb00f9f9aff3fd5873129548a3f5df6e0672db4
Nikhil-Gunne <94475590+Nikhil-Gunne@users.noreply.github.com>
2026-06-30 20:52:46 +0530

    Time: 648 ms (35.73%), Space: 33 MB (63.12%) - LeetHub

---
+++ 3739-count-subarrays-with-majority-element-ii/3739-count-subarrays-with-majority-element-ii.py
@@ -0,0 +1,17 @@
+
+from sortedcontainers import SortedList
+
+class Solution:
+    def countMajoritySubarrays(self, nums: List[int], target: int) -> int:
+        n = len(nums)
+        sl = SortedList()
+        sl.add(0)  # P[0] = 0
+        
+        cnt = 0
+        P = 0
+        for k in range(n):
+            P += 1 if nums[k] == target else -1
+            # count of previous prefix sums strictly less than P
+            cnt += sl.bisect_left(P)
+            sl.add(P)
+        return cnt
