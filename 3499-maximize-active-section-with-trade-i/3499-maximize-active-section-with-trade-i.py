994dbb40e6f3bc51cdbe739168c1b53c1d18640b
Nikhil-Gunne <94475590+Nikhil-Gunne@users.noreply.github.com>
2026-07-21 12:26:29 +0530

    Time: 503 ms (95.33%), Space: 20.2 MB (100%) - LeetHub

---
+++ 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.py
@@ -0,0 +1,26 @@
+class Solution:
+    def maxActiveSectionsAfterTrade(self, s: str) -> int:
+
+        prevBlock = 0
+        currBlock =0
+        onesBlock = 0
+        res = 0
+        totalOnes = s.count('1')
+        s+='1'
+        for i in s:
+            if i == '0':
+                currBlock += 1
+            else:
+                if prevBlock > 0 and currBlock > 0  and onesBlock > 0:
+                    res = max(res,prevBlock+currBlock+onesBlock+(totalOnes-onesBlock))
+                    onesBlock = 0
+
+                if currBlock > 0:
+                    prevBlock = currBlock
+                    currBlock = 0
+                onesBlock += 1
+        return max(res,totalOnes)
+
+
+
+        
