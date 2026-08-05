39d29faf87379b915c93105fb2120654e2f3aeee
Nikhil-Gunne <94475590+Nikhil-Gunne@users.noreply.github.com>
2026-08-05 09:29:33 +0530

    Time: 283 ms (71.97%), Space: 106.8 MB (59.85%) - LeetHub

--- 3310-remove-methods-from-project/3310-remove-methods-from-project.py
+++ 3310-remove-methods-from-project/3310-remove-methods-from-project.py
@@ -24,45 +24,45 @@ class Solution:
             return []
         
         
-        # for u, v in invocations:
-        #     if u not in suspiciousMethods and v in suspiciousMethods:
-        #         return list(range(n))
+        for u, v in invocations:
+            if u not in suspiciousMethods and v in suspiciousMethods:
+                return list(range(n))
 
         
-        # return [i for i in range(n) if i not in suspiciousMethods]
+        return [i for i in range(n) if i not in suspiciousMethods]
 
-        def bfs(node):
-            q = deque([node])
+        # def bfs(node):
+        #     q = deque([node])
             
-            f = 0
-            while q:
-                curr = q.popleft()
+        #     f = 0
+        #     while q:
+        #         curr = q.popleft()
 
-                if curr in suspiciousMethods:
-                    return True
+        #         if curr in suspiciousMethods:
+        #             return True
                 
-                if curr in v1:
-                    continue
-                v1.add(curr)
-                for nei in graph[curr]:
-                    if nei not in v1:
-                        q.append(nei)
-            return False
-        v1 = set()
-        f= 0
-        for i in range(n):
-            if i not in suspiciousMethods and i not in v1:
-                temp = bfs(i)
-                if temp:
-                    f = 1
-                    break
-        res = []
-        if f:
-            return list(range(n))
-        for i in range(n):
-            if i not in suspiciousMethods:
-                res.append(i)
-        return res            
+        #         if curr in v1:
+        #             continue
+        #         v1.add(curr)
+        #         for nei in graph[curr]:
+        #             if nei not in v1:
+        #                 q.append(nei)
+        #     return False
+        # v1 = set()
+        # f= 0
+        # for i in range(n):
+        #     if i not in suspiciousMethods and i not in v1:
+        #         temp = bfs(i)
+        #         if temp:
+        #             f = 1
+        #             break
+        # res = []
+        # if f:
+        #     return list(range(n))
+        # for i in range(n):
+        #     if i not in suspiciousMethods:
+        #         res.append(i)
+        # return res            
 
             
 
