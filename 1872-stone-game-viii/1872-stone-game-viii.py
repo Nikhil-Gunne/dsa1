3af18a39acd302aca3ee06e8e0e7b0a04cff195a
Nikhil-Gunne <94475590+Nikhil-Gunne@users.noreply.github.com>
2026-08-24 11:33:40 +0530

    Time: 710 ms (28.4%), Space: 33.2 MB (49.7%) - LeetHub

--- 1872-stone-game-viii/1872-stone-game-viii.py
+++ 1872-stone-game-viii/1872-stone-game-viii.py
@@ -8,21 +8,31 @@ class Solution:
         
         for i in range(1,n):
             pref[i] = pref[i-1] + stones[i] 
-            
-
-        dp = [float('-inf')] * n
-        def solve(idx):
-            if idx == n-1:
-                return pref[idx]
-            
-            if dp[idx] != float('-inf'):
-                return dp[idx]
-            
-            take = pref[idx] - solve(idx+1)
-            skip = solve(idx+1)
+        
+        dp = [0] * n
+        dp[-1] = pref[-1]
+        for idx in range(n-2,-1,-1):
+            take = pref[idx] - dp[idx+1]
+            skip = dp[idx+1]
             dp[idx] = max(take,skip)
-            return dp[idx]
-        return solve(1)
+        return dp[1]
+
+        # dp = [float('-inf')] * n
+        # def solve(idx):
+        #     if idx == n-1:
+        #         return pref[idx]
+            
+        #     if dp[idx] != float('-inf'):
+        #         return dp[idx]
+            
+        #     take = pref[idx] - solve(idx+1)
+        #     skip = solve(idx+1)
+        #     dp[idx] = max(take,skip)
+        #     return dp[idx]
+        # return solve(1)
+
+        
+
         
 
 
